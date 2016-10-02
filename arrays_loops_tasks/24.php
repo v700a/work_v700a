735a21e3ba87cffe194da1b7057a0e20899f85a7
v700a <v700a@ukr.net>
2016-10-02 13:44:42 +0300

    Add files via upload

--- arrays_loops_tasks/24.php
+++ arrays_loops_tasks/24.php
@@ -7,11 +7,16 @@
 <body>
 
 <b>
-    <form action="23.php" method="post">
-        Введіть будь-яке багатозначне число і отримайте суму його чисел:<br>
+    <form action="24.php" method="post">
+        Введіть будь-яке багатозначне число:<br>
         <br>
         <input type="number" name="n_1" placeholder="Введіть багатозначне число" autofocus>
         <br><br>
+        Введіть будь-яку цифру для знаходження кількості входжень у введене число:<br>
+        <br>
+        <input type="text" name="n_2" maxlength="1" placeholder="Введіть цифру">
+        <br><br>
+        <input type="submit">
     </form>
 
 
@@ -21,26 +26,32 @@
     }
 
 
-    echo '<br><br>';
-    echo 'Введене число рівне - ( ' ;
-    echo '</b>';
     $a = $_POST['n_1'];
-    echo "$a )";
-    $b = (string)$a;
-    $c = 0;
+    $b = $_POST['n_2'];
+    echo '<br><br>';
+    if (!is_numeric($b)) :
+    echo "Введений символ не є цифрою. Введіть, будь ласка, цифру.";
+    $b = 'Не цифра!';
+    endif;
+    echo '<br><br>';
+    echo "Введене число - ( $a )" ;
+    echo '<br><br>';
+    echo "Введене цифра - ( $b )" ;
+    $c = (string)$a;
+    $d = 0;
     echo '<br><br>';
 
-    for ($i = 0; (@(bool)$b[$i] !== false); $i = $i+1 ) :
-
-        $c = $c + $b[$i];
-
+    for ($i = 0; (@(bool)$c[$i] !== false); $i = $i+1 ) :
+        if ($c[$i] == $b) {
+        $d = $d + 1;
+        }
     endfor;
 
     echo '<br><br>';
 
-    echo 'Результат доавання чисел:';
+    echo "Кількість входжень цифри ( {$b} ) у число ( {$a} ) рівне - ( {$d} )";
     echo '<br><br>';
-    echo $c;
+    echo '</b>';
 
     ?>
 </b>
