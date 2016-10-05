ee7730cfb0006c9431d488d4845c0216683fb927
v700a <v700a@ukr.net>
2016-10-05 18:21:05 +0300

    Add files via upload

--- arrays_loops_tasks/26.php
+++ arrays_loops_tasks/26.php
@@ -8,54 +8,41 @@
 <b>
     <?php
     echo 'Задано масив за допомогою функції - rand:';
-    echo '<br><br>';
+    echo '<br>';
     $massyv = array();
 
-    for ($i = 0; $i <= 100; $i++) :
-        $a = rand(1, 200);
+    for ($i = 0; $i <= 10; $i++) :
+        $a = rand(0, 100);
         $massyv [] = $a;
     endfor;
 
     echo '<pre>';
     print_r($massyv);
     echo '</pre>';
-    echo '<br><br>';
-    echo 'Знайти добуток трьох елементів більших нуля і парними індексами:';
-    echo '<br><br>';
+//    echo '<br>';
+    echo 'Знайти добуток тих елементів більших нуля і парними індексами:';
+    echo '<br>';
     echo '<br><br>';
     echo 'Список елементів більших нуля і парними індексами:';
     echo '<br><br>';
-    $b = 0;
+    $b = 1;
     $c = 0;
     $d = 0;
     $e = 0;
     $f = 0;
-    $g = 0;
 
     foreach ($massyv as $key =>$element1) :
             if ($element1 > 0 && $key % 2 == 0) :
-                if ($b == 0) {
-                    $c = $element1;
+                    $b = $c * $element1;
+                    $c = $key;
                     $d = $element1;
-                    $f = $key;
-                    echo "Ключ - ({$f}). число - ({$d}) <br><br>";
-                }
-                else {
-                    $c = $c * $element1;
-                    $f = $key;
-                    $d = $element1;
-                    echo "Ключ - ({$f}). число - ({$d}) <br><br>";
-                }
-                $b++;
-            endif;
-            if ($b == 3) :
-                break;
+                    echo "Ключ - ({$c}). число - ({$d}) <br><br>";
             endif;
     endforeach;
 
     echo 'Добуток вищевказаних елементів становить - ';
     echo $c;
-    echo '<br><br>';
+    echo '<br>';
     echo '<br><br>';
     echo 'Список елементів більших нуля і непарними індексами:';
     echo '<br><br>';
@@ -63,8 +50,8 @@
     foreach ($massyv as $key =>$element1) :
             if ($element1 > 0 && $key % 2 == 1) :
                     $e = $element1;
-                    $g = $key;
-                    echo "Ключ - ({$g}). число - ({$e}) <br><br>";
+                    $f = $key;
+                    echo "Ключ - ({$e}). число - ({$f}) <br><br>";
             endif;
     endforeach;
 ?>
